89d1fae8fb41ced89a11d184eaab0a406a3207b0
Hashir Ahmad <hashirahmad@zhcet.ac.in>
2024-10-14 23:48:41 +0200

    ex2.11: add partial solution of part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, newSearch] = useState('')
+
+  // add effect hook to initialize the persons array
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', persons.length, 'entries');
 
   const addName = (event) => {
     event.preventDefault()
     const person = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: String(persons.length + 1)
     }
 
     let duplicate = false
