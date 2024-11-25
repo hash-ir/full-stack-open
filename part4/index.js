be170a3187a800d0c1f70e79fd93e5bfda014bf0
hash-ir <hashirahmad@zhcet.ac.in>
2024-11-25 10:53:49 +0530

    refactor: initial project directory restructuring
    This commit introduces the initial project structure with the following changes:
    - Split 'index.js' into core application files 'app.js', 'models', 'utils'
    - Created placeholder files for future implementation 'utils/logger.js', 'utils/middleware.js'

--- part4/index.js
+++ part4/index.js
@@ -1,23 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
+const app = require('./app')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -37,7 +20,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
