18f1b34f2a7014ffc7babd6e36a6dcc505de2169
hash-ir <hashirahmad@zhcet.ac.in>
2024-11-25 11:33:53 +0530

    feat: add logger and middlewares

--- part4/app.js
+++ part4/app.js
@@ -2,19 +2,39 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
-        console.log('connected to MongoDB')
+        logger.info('connected to MongoDB')
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
+        logger.error('error connecting to MongoDB:', error.message)
     })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+    blog.save().then(result => {
+        response.status(201).json(result)
+    })
+})
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,25 +1,7 @@
 const app = require('./app')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
