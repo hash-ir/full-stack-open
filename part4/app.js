8d17fb6256de7e4c94879f4ef3fe2acebc27ed93
hash-ir <hashirahmad@zhcet.ac.in>
2024-11-25 20:19:52 +0530

    ex4.1-4.2: add partial solution of part4

--- part4/app.js
+++ part4/app.js
@@ -2,9 +2,9 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -21,18 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save().then(result => {
-        response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
