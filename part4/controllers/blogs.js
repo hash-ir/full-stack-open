daaad91d71a88256136da5a53ceec6cd15dafd98
hash-ir <hashirahmad@zhcet.ac.in>
2025-01-12 14:35:14 +0100

    ex4.19: add partial solution of part4

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,12 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
         .populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
@@ -15,14 +24,19 @@ blogsRouter.post('/', async (request, response) => {
         response.status(400).send()
     }
 
-    const user = await User.findOne()
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
         url: body.url,
         author: body.author,
         likes: body.likes === undefined ? 0 : body.likes,
-        user: user.id
+        user: user._id
     })
 
     const savedBlog = await blog.save()
