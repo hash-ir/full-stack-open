b607c64c9a43f31521c56112ccfcc38cf07e1278
hash-ir <hashirahmad@zhcet.ac.in>
2025-01-22 21:18:22 +0100

    ex5.3: add partial solution of part5

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blogs from './components/Blogs'
 import Login from './components/Login'
+import Create from './components/Create'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -9,6 +10,11 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: ''
+  })
 
   useEffect(() => {
     const loggedUserJson = window.localStorage.getItem('loggedUser')
@@ -46,15 +52,30 @@ const App = () => {
     }
   }
 
+  const handleCreateBlog = async (event) => {
+    // event.preventDefault()
+    try {
+      blogService.setToken(user.token)
+      blogService.create(newBlog)
+      blogService.getAll().then(blogs => 
+        setBlogs(blogs)
+      )
+    } catch (error) {
+      console.error('Blog could not be added:', error.response ? error.response.data : error.message)
+    }
+    
+  }
+
   return (
     <div>
-      {user 
-        ? Blogs(user.name, blogs, handleLogout)
-        : Login(
-          {username, password}, 
-          {setUsername, setPassword},
-          handleLogin
-        )
+      {user === null ?
+        Login({username, password}, {setUsername, setPassword}, handleLogin) :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.username} logged in<button onClick={handleLogout}>logout</button></p>
+          {Create(newBlog, setNewBlog, handleCreateBlog)}
+          {Blogs(user.name, blogs, handleLogout)}
+        </div> 
       }
     </div>
   )
---
+++ part5/src/components/Create.jsx
@@ -0,0 +1,53 @@
+const Create = (newBlog, setNewBlog, handleCreateBlog) => {
+    const { title, author, url } = newBlog
+    // const { setTitle, setAuthor, setUrl} = addNewBlog
+    return (
+        <div>
+            <form onSubmit={handleCreateBlog}>
+                <h2>create new</h2>
+                <div>
+                    title
+                    <input 
+                        type="text" 
+                        value={title}
+                        name="Title"
+                        onChange={({ target }) => 
+                            setNewBlog({
+                                ...newBlog, 
+                                title: target.value
+                            })}
+                    />
+                </div>
+                <div>
+                    author
+                    <input 
+                        type="text" 
+                        value={author}
+                        name="Author"
+                        onChange={({ target }) => 
+                            setNewBlog({
+                                ...newBlog,
+                                author: target.value
+                            })}
+                    />
+                </div>
+                <div>
+                    url
+                    <input 
+                        type="text" 
+                        value={url}
+                        name="URL"
+                        onChange={({ target }) => 
+                            setNewBlog({
+                                ...newBlog,
+                                url: target.value
+                            })}
+                    />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    )
+}
+
+export default Create
