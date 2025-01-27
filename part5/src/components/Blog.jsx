80ba7f3e7cf9870b5375e3de28e712e547439476
hash-ir <hashirahmad@zhcet.ac.in>
2025-01-27 15:24:16 +0100

    ex5.7: add partial solution of part5

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonLabel = viewDetails ? 'hide' : 'view'
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setViewDetails(!viewDetails)}>{buttonLabel}</button>
+      {
+        viewDetails && 
+        <div>
+          {blog.url} <br />
+          {blog.likes} <button>like</button> <br />
+          {blog.author}
+        </div>
+      }
+    </div>  
+  )
+}
 
 export default Blog
