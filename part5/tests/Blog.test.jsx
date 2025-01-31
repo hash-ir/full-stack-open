90c7f3e312e9ea14510c87369300778c36939e9d
hash-ir <hashirahmad@zhcet.ac.in>
2025-01-31 03:23:43 +0100

    ex5.15: add partial solution of part5

--- part5/tests/Blog.test.jsx
+++ part5/tests/Blog.test.jsx
@@ -2,10 +2,12 @@ import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { beforeEach, describe } from 'vitest'
 import Blog from '../src/components/Blog'
+import blogService from '../src/services/blogs'
 
 describe('<Blog />', () => {
     let blog
-    let container 
+    let container
+    let mockUpdateBlogs 
 
     /* `beforeAll` does not work with this setup as the testing library
     cleans up the DOM after each test; re-rendering of the component is
@@ -31,11 +33,13 @@ describe('<Blog />', () => {
             name: 'Test User' 
         }
 
+        mockUpdateBlogs = vi.fn()
+
         // Get the containing DOM node (div)
         container = render(
             <Blog 
                 blog={blog} 
-                updateBlogs={() => {}} 
+                updateBlogs={mockUpdateBlogs} 
                 removeBlog={() => {}} 
                 loggedUser={loggedUser}
             />
@@ -55,8 +59,9 @@ describe('<Blog />', () => {
         expect(divBlogDetails).toBeNull()
     })
 
-    test('renders url and likes when \'view\' button is clicked', async () => {
+    test('clicking the view button renders url and likes', async () => {
         const user = userEvent.setup()
+        // Click the view button to show details
         const button = screen.getByRole('button', { name: 'view' })
         await user.click(button)
         expect(button).toHaveTextContent('hide')
@@ -65,6 +70,42 @@ describe('<Blog />', () => {
         const div = container.querySelector('.blog-details')
         // Performs a partial case-sensitive match when a string is passed
         expect(div).toHaveTextContent(`${blog.url} ${blog.likes}`)
+    })
+
+    test('clicking the like button twice calls event handler twice', async () => {
+        const user = userEvent.setup()
+        // First, click the view button to show details
+        const viewButton = screen.getByRole('button', { name: 'view' })
+        await user.click(viewButton)
+
+        // Mock the entire blog service to avoid actual API calls
+        vi.mock('../src/services/blogs', () => ({
+            default: {
+                // Mock the updateLikes function
+                updateLikes: vi.fn()
+            }
+        }))
+        
+        /* Mock the return value of updateLikes
+        mockResolvedValue is used here instead of mockReturnValue
+        since updateLikes is an async function */
+        blogService.updateLikes.mockResolvedValue({
+            ...blog,
+            likes: blog.likes + 1
+        })
+
+        // Find and click the like button twice
+        const likeButton = screen.getByRole('button', { name: 'like' })
+        await user.click(likeButton)
+        await user.click(likeButton)
+        
+        // Following checks should be enough to know that the event
+        // handler of like button was called twice 
+        // Check that the updateLikes service was called twice
+        expect(blogService.updateLikes.mock.calls).toHaveLength(2)
+
+        // Check that updateBlogs was called twice
+        expect(mockUpdateBlogs.mock.calls).toHaveLength(2)
     })
 })
 
