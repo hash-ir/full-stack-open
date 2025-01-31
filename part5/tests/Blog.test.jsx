8ff620aa056ad3ac22659aff4b33c84bf37d1d55
hash-ir <hashirahmad@zhcet.ac.in>
2025-01-31 02:48:11 +0100

    ex5.14: add partial solution of part5

--- part5/tests/Blog.test.jsx
+++ part5/tests/Blog.test.jsx
@@ -1,11 +1,21 @@
-import { render } from '@testing-library/react'
-import { describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { beforeEach, describe } from 'vitest'
 import Blog from '../src/components/Blog'
 
-
 describe('<Blog />', () => {
-    test('renders title and author but not url or likes by default', () => {
-        const blog = {
+    let blog
+    let container 
+
+    /* `beforeAll` does not work with this setup as the testing library
+    cleans up the DOM after each test; re-rendering of the component is
+    required 
+    
+    `beforeEach` is the easiest fix and even more idiomatic in react
+    testing library. Besides, the setup isn't expensive enough to
+    warrant using `beforeAll`*/
+    beforeEach(() => {
+        blog = {
             title: 'Component testing is done with react-testing-library',
             author: 'Test Author',
             url: 'https://testing-library.com/',
@@ -20,17 +30,20 @@ describe('<Blog />', () => {
             username: 'testuser',
             name: 'Test User' 
         }
-        
+
         // Get the containing DOM node (div)
-        const { container } = render(
+        container = render(
             <Blog 
                 blog={blog} 
                 updateBlogs={() => {}} 
                 removeBlog={() => {}} 
                 loggedUser={loggedUser}
             />
-        )
+        ).container
 
+    })
+
+    test('renders title and author but not url or likes by default', () => {
         // Check that title and author are rendered as expected
         const divBlog = container.querySelector('.blog')
         expect(divBlog).toHaveTextContent(
@@ -40,6 +53,18 @@ describe('<Blog />', () => {
         // Check that URL and likes are not rendered
         const divBlogDetails = container.querySelector('.blog-details')
         expect(divBlogDetails).toBeNull()
+    })
+
+    test('renders url and likes when \'view\' button is clicked', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByRole('button', { name: 'view' })
+        await user.click(button)
+        expect(button).toHaveTextContent('hide')
+
+        // Check that URL and likes are rendered
+        const div = container.querySelector('.blog-details')
+        // Performs a partial case-sensitive match when a string is passed
+        expect(div).toHaveTextContent(`${blog.url} ${blog.likes}`)
     })
 })
 
