8b599ac0bd856a22f971b934257c15fa53dce483
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-02 23:57:33 +0100

    ex5.18: add partial solution of part5

--- part5-tests/tests/blog_app.spec.js
+++ part5-tests/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+        data: {
+            name: 'Test User',
+            username: 'tester',
+            password: 'vois-sur-ton-chemin'
+        }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,5 +19,26 @@ describe('Blog app', () => {
     await expect(page.getByRole('textbox').first()).toBeVisible()
     await expect(page.getByRole('textbox').last()).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('tester')
+        await page.getByTestId('password').fill('vois-sur-ton-chemin')
+        await page.getByRole('button', { name: 'login' }).click()
+        await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('tester')
+        await page.getByTestId('password').fill('wrong')
+        await page.getByRole('button', { name: 'login' }).click()
+        
+        const errorDiv = await page.locator('.error')
+        await expect(errorDiv).toContainText('invalid username or password')
+        await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+        
+        await expect(page.getByText('Test User logged in')).not.toBeVisible()
+    })
   })
 })
