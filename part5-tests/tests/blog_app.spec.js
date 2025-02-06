627e2e40fc1c0568d1bfb3edf23834b7537bb793
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-06 23:03:46 +0100

    ex5.20: add partial solution of part5

--- part5-tests/tests/blog_app.spec.js
+++ part5-tests/tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith } = require('./helper')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -45,38 +45,54 @@ describe('Blog app', () => {
     })
 
     test('a new blog can be created', async ({ page }) => {
-        await page.getByRole('button', { name: 'create new blog' }).click()
-        await page.getByTestId('title').fill('A blog created by Playwright')
-        await page.getByTestId('author').fill('Wright Brothers')
-        await page.getByTestId('url').fill('example.com/blogs/playwright')
-        await page.getByRole('button', { name: 'create' }).click()
+      const blog = {
+        title: 'A blog created by Playwright',
+        author: 'Wright Brothers',
+        url: 'example.com/blogs/playwright'
+      }
 
-        const blogDiv = await page.locator('.blog')
-        await expect(blogDiv).toContainText('A blog created by Playwright')
-        
-        const successDiv = await page.locator('.success')
-        await expect(successDiv).toHaveCSS('color', 'rgb(0, 128, 0)')
+      await createBlog(page, blog)
+
+      const blogDiv = await page.locator('.blog')
+      await expect(blogDiv).toContainText('A blog created by Playwright')
+      
+      const successDiv = await page.locator('.success')
+      await expect(successDiv).toHaveCSS('color', 'rgb(0, 128, 0)')
     })
 
-    test('a blog can be liked', async ({ page }) => {
-        const blogElement = await page.locator('.blog')
-          .filter({ hasText: 'A blog created by Playwright' })
-          .waitFor()
-        await blogElement.getByRole('button', { name: 'view' }).click()
+    describe('and a blog exists', () => {
+      beforeEach(async ({ page }) => {
+        const blog = {
+          title: 'Another Blog by Playwright',
+          author: 'Wright Brothers',
+          url: 'example.com/blogs/playwright-2'
+        }
+
+        await createBlog(page, blog)
+      })
+
+      test.only('a blog can be liked', async ({ page }) => {
+        const blogContainer = await page.locator('.blog')
+          .filter({ hasText: 'Another Blog by Playwright'}) 
+        await blogContainer.getByRole('button', { name: 'view' }).click()
         const blogDetails = blogContainer.locator('.blog-details')
         const likesText = await blogDetails.locator('text=/\\d+/').first().textContent()
         // Get the initial likes value
         const initialLikes = parseInt(likesText, 10)
-
+        await blogDetails.getByText(likesText).waitFor()
+        
         // Click the like button
-        await blogElement.getByRole('button', { name: 'like' }).click()
-
+        await blogDetails.getByRole('button', { name: 'like' }).click()
+        
         // Check that the final likes value incremented by 1
         await expect(async () => {
           const newLikesText = await blogDetails.locator('text=/\\d+/').first().textContent()
+          // Wait for the new likes to appear
+          await blogDetails.getByText(newLikesText).waitFor()
           const newLikes = parseInt(newLikesText, 10)
           expect(newLikes).toBe(initialLikes + 1)
         }).toPass()
+      })
     })
   })
 })
