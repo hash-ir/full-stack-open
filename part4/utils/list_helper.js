b5eb0211953c828c930505facc20154177672200
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-11 16:03:48 +0100

    ex4.6-4.7: add partial solution of part4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -27,9 +27,53 @@ const favoriteBlog = (blogs) => {
 
     return result
 }
+
+const mostBlogs = (blogs) => {
+    const reducer = (acc, blog) => {
+        acc[blog.author] = (acc[blog.author] || 0) + 1
+        return acc
+    }
+
+    if (blogs.length == 0) {
+        return null
+    }
+
+    const blogCounts = blogs.reduce(reducer, {})
+    const authorWithMostBlogs = Object.entries(blogCounts).reduce((prev, curr) => {
+        return curr[1] > prev[1] ? curr : prev
+    })
+
+    return {
+        author: authorWithMostBlogs[0],
+        blogs: authorWithMostBlogs[1]
+    }
+}
+
+const mostLikes = (blogs) => {
+    const reducer = (acc, blog) => {
+        acc[blog.author] = (acc[blog.author] || 0) + blog.likes
+        return acc
+    }
+
+    if (blogs.length == 0) {
+        return null
+    }
+
+    const blogLikes = blogs.reduce(reducer, {})
+    const authorWithMostLikes = Object.entries(blogLikes).reduce((prev, curr) => {
+        return curr[1] > prev[1] ? curr : prev
+    })
+    
+    return {
+        author: authorWithMostLikes[0],
+        likes: authorWithMostLikes[1]
+    }
+}
   
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
