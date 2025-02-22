34bc259a5f0bfd4bb98017d251b298f266ee0652
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-22 14:53:51 +0100

    ex6.21: add partial solution of part6

--- part6/query-anecdotes/requests.js
+++ part6/query-anecdotes/requests.js
@@ -3,3 +3,7 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 
 export const getAnecdotes = () =>
   axios.get(baseUrl).then(res => res.data)
+
+export const createAnecdote = (object) => {
+    axios.post(baseUrl, object).then(res => res.data)
+}
