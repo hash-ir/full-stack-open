4446e3370b08146eec7a768573387fd118552fde
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-22 15:00:32 +0100

    ex6.22: add partial solution of part6

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,12 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from '../requests'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, voteAnecdote } from '../requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    voteAnecdoteMutation.mutate({ 
+      ...anecdote, 
+      votes: anecdote.votes + 1
+    })
   }
 
   const result = useQuery({
