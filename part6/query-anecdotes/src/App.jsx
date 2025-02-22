bbb8f6548e5e9672c371c0337ed6b9c796743099
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-22 14:45:58 +0100

    ex6.20: add partial solution of part6

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from '../requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
@@ -7,14 +9,29 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
 
+  if (result.isLoading) {
+    return (
+      <div>
+        loading anecdotes data...
+      </div>
+    )
+  }
+
+  if (result.isError) {
+    return (
+      <div>
+        anecdote service not available due to problems in server
+      </div>
+    )
+  }
+
+  // We can assume by this point that `isSuccess === true`
+  const anecdotes = result.data
   return (
     <div>
       <h3>Anecdote app</h3>
