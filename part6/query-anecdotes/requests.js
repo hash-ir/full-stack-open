3b9a0e6dd850923899b08bf792d156db0826fe66
hash-ir <hashirahmad@zhcet.ac.in>
2025-02-22 15:08:55 +0100

    fix: return correctly from axios request functions

--- part6/query-anecdotes/requests.js
+++ part6/query-anecdotes/requests.js
@@ -4,11 +4,9 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAnecdotes = () =>
     axios.get(baseUrl).then(res => res.data)
 
-export const createAnecdote = (newAnecdote) => {
+export const createAnecdote = (newAnecdote) => 
     axios.post(baseUrl, newAnecdote).then(res => res.data)
-}
 
-export const voteAnecdote = (votedAnecdote) => {
+export const voteAnecdote = (votedAnecdote) =>
     axios.put(`${baseUrl}/${votedAnecdote.id}`, votedAnecdote)
         .then(res => res.data)
-}
