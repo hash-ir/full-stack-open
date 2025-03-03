257fe3039cc164a81737e188ed30ee7f7ea75417
hash-ir <hashirahmad@zhcet.ac.in>
2025-03-03 23:39:45 +0100

    chore: create backend and frontend dirs

---
+++ part7/redux-bloglist/backend/controllers/users.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+        .populate('blogs', { url: 1, title: 1, author: 1 })
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+    const { username, name, password } = request.body
+
+    // check if username already exists
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+        return response.status(400).json({
+            error: 'expected `username` to be unique'
+        })
+    }
+
+    // check if password length is greater than 3
+    if (password.length < 3) {
+        return response.status(400).json({
+            error: '`password` must be at least 3 characters long'
+        })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username: username,
+        name: name,
+        password: passwordHash
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
