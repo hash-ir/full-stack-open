eb6940d1d15cc7cd62508409204074121d040a8c
hash-ir <hashirahmad@zhcet.ac.in>
2025-03-25 23:59:13 +0100

    ex8.6: add partial solution of part8

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -117,6 +118,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      author: String!,
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -147,6 +157,14 @@ const resolvers = {
         ).length
         return { ...author, bookCount }
       }),
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+
+      return book
+    },
   },
 }
 
