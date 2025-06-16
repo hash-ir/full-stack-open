30c34bd204516fb4c885b62954237ff1d9fd3123
hash-ir <hashirahmad@zhcet.ac.in>
2025-06-16 13:06:54 +0200

    ex9.5: add partial solution of part9

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters: number = height / 100;
   if (heightInMeters === 0) {
     throw new Error("Height must not be 0.");
@@ -18,28 +18,30 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-const bmiArgs = process.argv.slice(2);
+if (require.main === module) {
+  const bmiArgs = process.argv.slice(2);
 
-if (bmiArgs.length !== 2) {
-  console.error(
-    `Expected exactly 2 number arguments (height (cm), width (kg)). Found ${
-      process.argv.length - 2
-    }`
-  );
-  process.exit(1);
-}
-
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
-
-try {
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong: ";
-
-  if (error instanceof Error) {
-    errorMessage += error.message;
+  if (bmiArgs.length !== 2) {
+    console.error(
+      `Expected exactly 2 number arguments (height (cm), width (kg)). Found ${
+        process.argv.length - 2
+      }`
+    );
+    process.exit(1);
   }
 
-  console.log(errorMessage);
+  const height: number = Number(process.argv[2]);
+  const weight: number = Number(process.argv[3]);
+
+  try {
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong: ";
+
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+
+    console.log(errorMessage);
+  }
 }
--- part9/index.ts
+++ part9/index.ts
@@ -1,11 +1,53 @@
 import express from "express";
 import dotenv from "dotenv";
+import { calculateBmi } from "./bmiCalculator";
 
 const app = express();
 dotenv.config();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const heightParam = req.query.height as string;
+  const weightParam = req.query.weight as string;
+  const errors: string[] = [];
+
+  if (!heightParam) errors.push("height is required");
+  if (!weightParam) errors.push("weight is required");
+
+  // return early if parameters are missing
+  if (errors.length > 0) {
+    console.error("Missing parameters:", errors.join(", "));
+    res.send({ error: `malformatted parameters: ${errors.join(", ")}` });
+    return;
+  }
+
+  // check for valid numbers
+  const height: number = Number(heightParam);
+  const weight: number = Number(weightParam);
+
+  if (isNaN(height)) {
+    errors.push(`height '${heightParam}' is not a valid number`);
+  }
+
+  if (isNaN(weight)) {
+    errors.push(`weight '${weightParam}' is not a valid number`);
+  }
+
+  if (errors.length > 0) {
+    console.error("Invalid parameters:", errors.join(", "));
+    res.send({ error: `malformatted parameters: ${errors.join(", ")}` });
+    return;
+  }
+
+  const bmi = calculateBmi(height, weight);
+  res.send({
+    weight,
+    height,
+    bmi,
+  });
 });
 
 const PORT = process.env.PORT;
